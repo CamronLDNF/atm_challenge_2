9269d3b02260a09c99df8ec8b3da270705adc5d5
camron <camronmoradi@gmail.com>
2018-12-21 19:00:16 +0700

    Adds test for account owner and adds method for assigning an owner attribute to account

--- lib/account.rb
+++ lib/account.rb
@@ -3,13 +3,13 @@ require 'date'
 class Account
   STANDARD_VALIDITY_YRS = 5
 
-  # attr_accessor :pin_code
-  attr_reader :pin_code, :exp_date, :account_status
+  attr_reader :pin_code, :exp_date, :account_status, :owner
 
-  def initialize
+  def initialize (attrs = {})
     @pin_code = rand(1000..9999)
     @exp_date = set_expire_date
     @account_status = :active
+    set_owner(attrs[:owner])
   end
 
   def set_expire_date
@@ -18,6 +18,16 @@ class Account
 
   def deactivate
     @account_status = :disabled
+  end
+
+  private
+
+  def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise 'An Account owner is required'
   end
 
 end
